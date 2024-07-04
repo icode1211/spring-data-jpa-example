046a8a44ddb6abde7ed0d1d406e5ac0d687758de
icode1211 <icode1211@naver.com>
2024-07-04 16:48:15 +0900

    add: @Transactional 어노테이션 추가

--- src/main/java/com/example/springdatajpaexample/service/StudentService.java
+++ src/main/java/com/example/springdatajpaexample/service/StudentService.java
@@ -5,6 +5,7 @@ import com.example.springdatajpaexample.domain.Students;
 import com.example.springdatajpaexample.repository.ClassesRepository;
 import com.example.springdatajpaexample.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class StudentService {
         return studentRepository.findByName(name);
     }
 
+    @Transactional
     public Students insertStudent(Students student) {
         return studentRepository.save(student);
     }
